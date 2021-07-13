f199a64e7ef3fc8fc8097d2518c1bee130566fbf
nohssam <nohssam73@gmail.com>
2021-07-13 13:06:00 +0900

    second

--- src/main/java/com/ict/dao/MyDAOImpl.java
+++ src/main/java/com/ict/dao/MyDAOImpl.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.ict.vo.CVO;
+import com.ict.vo.TVO;
 import com.ict.vo.VO;
 
 @Repository("myDAOImpl")
@@ -13,12 +15,19 @@ public class MyDAOImpl implements MyDAO{
 	
 	@Override
 	public void insertCard(VO vo) throws Exception {
-		// TODO Auto-generated method stub
-		
+		sqlSessionTemplate.insert("tx.cardInsert", vo);
 	}
 	@Override
 	public void insertTicket(VO vo) throws Exception {
-		// TODO Auto-generated method stub
-		
+		sqlSessionTemplate.insert("tx.ticketInsert", vo);
+	}
+	
+	@Override
+	public void insertCard(CVO cvo) throws Exception {
+		sqlSessionTemplate.insert("tx.cardInsert2", cvo);
+	}
+	@Override
+	public void insertTicket(TVO tvo) throws Exception {
+		sqlSessionTemplate.insert("tx.ticketInsert2", tvo);
 	}
 }
